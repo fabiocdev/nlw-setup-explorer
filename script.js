4a03adc632df52ed9e71534a027b00832fb09e7d
fabiocdev <devfedc1101@gmail.com>
2023-01-22 22:08:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("🛑 Dia já incluso")
+    return
+  }
+
+  alert("✅ Adicionado com sucesso")
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
+
+/*const data = {
+  run: ["01-21", "01-22", "01-23", "01-24", "01-25", "01-29"],
+  water: ["01-21"],
+  study: ["01-21", "01-22"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
+*/
